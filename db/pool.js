05a586b7b19b3ba4ce38535cfd3921e2d3ad8402
EmmaKespo <kespoemma@gmail.com>
2026-06-24 10:19:35 +0100

    changed connection string settings to match .env file

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,9 @@
-const { Pool } = require('pg');
 require('dotenv').config(); // Loads environment variables
+const { Pool } = require('pg');
+
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+   connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
